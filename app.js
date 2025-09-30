df376a6582f986fdaac1749d2c01c411773f8692
Andrii <andrejveselyj90@gmail.com>
2025-09-30 20:41:44 +0300

    removed config creation

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import fs from "fs/promises";
 import path from "path";
-import { jsonrepair } from "jsonrepair";
 
 const excludedFolders = [
     "node_modules",
@@ -92,80 +91,6 @@ async function getParsersFoldersFromSurname(surname) {
     return folders;
 }
 
-async function safeReadFile(path, encoding = "utf-8") {
-  try {
-    const content = await fs.readFile(path, encoding);
-    
-    return content;
-  } catch (err) {
-    console.log(err)
-
-    return null; 
-  }
-}
-
-async function createConfigFiles(dir) {
-    const objArr = await getDefaultConfigFile(dir);
-    const obj = Array.isArray(objArr) ? objArr?.at(objArr.length - 1) : objArr;
-
-    const brandFile = path.join(dir, 'brand.ts')
-    const paramsFile = path.join(dir, 'params.ts')
-
-    const emptyTemplate = `
-export default {
-    
-}
-`;
-
-    const brandTemplate = obj ? `
-export default {
-    brandId: '${obj.brandId}',
-    brandName: '${obj.brandName}',
-    brandNameSlug: '${obj.brandNameSlug}'
-}
-` : emptyTemplate;
-
-    await fs.writeFile(brandFile, brandTemplate.trimStart());
-    await fs.writeFile(paramsFile, emptyTemplate.trimStart());
-}
-
-async function getDefaultConfigFile(dir) {
-    const defaultFileTs = path.join(dir, 'default.ts');
-    const defaultFileJs = path.join(dir, 'default.js');
-
-    const code = await safeReadFile(defaultFileTs) || await safeReadFile(defaultFileJs);
-
-    if (!code) {
-        return null
-    }
-
-    let lines = code.split('\n');
-
-    lines = lines
-        // Remove require/import
-        .filter(line => !line.includes('require') && !line.includes('import') && !line.includes('export default'))
-        // Replace export statements with nothing
-        .map(line => line.replace(/^\s*(const|let|var|module\.exports|export\s*=\s*|exports\s*=)\s*[^{]*{?/, '{'))
-        // Remove JS expressions / template literals
-        .map(line => line.replace(/`[^`]*`/g, '"PLACEHOLDER_PATH"'))
-        .map(line => line.replace(/path\.resolve\([^)]*\)/g, '"PLACEHOLDER_PATH"'))
-        // Remove semicolons
-        .map(line => line.replace(/;\s*$/, ''))
-        // Fix single quotes to double quotes
-        .map(line => line.replace(/'([^']*)'/g, '"$1"'))
-        // Quote keys
-        .map(line => line.replace(/(\w+)\s*:/g, '"$1":'))
-        // Fix malformed URLs
-        .map(line => line.replace(/"https":\/\//g, 'https://'))
-        // Remove any leading = signs
-        .map(line => line.replace(/^\s*=\s*/, ''));
-    
-    const newCode = '{\n' + lines.join('\n') + '\n}';
-    const obj = JSON.parse(jsonrepair(newCode));
-
-    return obj;
-}
-
 function getOutputFilePath(file, outDir) {
     let splittedPath = file.split('/');
 
@@ -186,7 +111,7 @@ async function main() {
     const surnames = (await getSubfolders(rootDir));
 
     for (const surname of surnames) {
-        const parsersFolders = (await getParsersFoldersFromSurname(surname));
+        const parsersFolders = await getParsersFoldersFromSurname(surname);
 
         for (const parserFolder of parsersFolders) {
             const files = await getParserFolderContent(parserFolder)
@@ -195,10 +120,6 @@ async function main() {
                 const newFilePath = getOutputFilePath(file, outDir);
 
                 await fs.cp(file, newFilePath, { recursive: true });
-
-                if (newFilePath.includes('config')) {
-                    await createConfigFiles(newFilePath)
-                }
             }
         }
     }
